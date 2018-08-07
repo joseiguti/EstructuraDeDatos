69726f84844aee85ede5c6126c51274281fa7ed3
José Ignacio Gutiérrez Guzmán <joselitosinmiedo@gmail.com>
2018-08-06 21:45:06 -0500

    Cambios

--- Ejercicios/main.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  main.cpp
-//  EstructuraDeDatos
-//
-//  Created by José Ignacio Gutiérrez Guzmán on 8/6/18.
-//  Copyright © 2018 José Ignacio Gutiérrez Guzmán. All rights reserved.
-//
-
-#include "main.hpp"
