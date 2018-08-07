aa56d5903d1a41767b4bae3e25279b2f13dd1170
José Ignacio Gutiérrez Guzmán <joselitosinmiedo@gmail.com>
2018-08-06 19:48:04 -0500

    Initial Commit

---
+++ EstructuraDeDatos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EstructuraDeDatos
+//
+//  Created by José Ignacio Gutiérrez Guzmán on 8/6/18.
+//  Copyright © 2018 José Ignacio Gutiérrez Guzmán. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
