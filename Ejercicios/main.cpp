8e01812f91c073d7cbe6afa05f5cd2fde4701433
José Ignacio Gutiérrez Guzmán <joselitosinmiedo@gmail.com>
2018-08-06 21:44:07 -0500

    Nuevos archivos

---
+++ Ejercicios/main.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  EstructuraDeDatos
+//
+//  Created by José Ignacio Gutiérrez Guzmán on 8/6/18.
+//  Copyright © 2018 José Ignacio Gutiérrez Guzmán. All rights reserved.
+//
+
+#include "main.hpp"
---
+++ Ejercicios/main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  EstructuraDeDatos
+//
+//  Created by José Ignacio Gutiérrez Guzmán on 8/6/18.
+//  Copyright © 2018 José Ignacio Gutiérrez Guzmán. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
